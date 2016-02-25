387c777442c5ecbef2827a1e84fe8573edefa60e
brenodamata <bjrdamata@gmail.com>
2016-02-25 14:08:05 -0500

    Add associations to models

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,6 @@ class Article < ActiveRecord::Base
 	validates :title, presence: true
 	validates :description, presence: true
 
+	has_many :comments
 	belongs_to :user
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,8 @@
 class Comment < ActiveRecord::Base
+	validates :user, presence: true
+	validates :article, presence: true
+	validates :description, presence: true
+
   belongs_to :user
+  belongs_to :article
 end
