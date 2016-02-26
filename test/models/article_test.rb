33d594277ab4cc5f83feb64b011c83ada99b8efd
brenodamata <bjrdamata@gmail.com>
2016-02-26 15:02:54 -0500

    Add categories and tests

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+	def setup
+		@user = User.new(email: 'a@b.c', username: 'abc', password: "123123")
+		@article = Article.new(title: "Test Article", 
+													 description: "Article desc", 
+													 user: @user)
+	end
+
+
+	test "article should be valid" do
+		assert @article
+	end
+
+	test "article should have title" do
+		@article.title = " "
+		assert_not @article.valid?
+	end
+
+	test "article should have description" do
+		@article.description = " "
+		assert_not @article.valid?
+	end
+
+	test "article should belog to a user" do
+		@article.user = nil
+		assert_not @article.valid?
+	end
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,73 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+		@user = User.new(email: 't@b.c', username: 'tester', password: "123123")
+	end
+
+	test 'user should be valid' do
+		assert @user
+	end
+
+	test 'user should have a username' do
+		@user.username = " "
+		assert_not @user.valid?
+	end
+
+	test 'username should be unique' do 
+		@user.save
+		new_user = User.new(email: 't1@b.c', username: 'tester', password: "123123")
+		assert_not new_user.valid?
+	end
+
+	test 'username uniqueness should not be case sensitive' do
+		@user.save
+		new_user = User.new(email: 't2@b.c', username: 'TesTer', password: "123123")
+		assert_not new_user.valid?
+	end
+
+	test 'username should not be too long' do
+		@user.username = "a" * 26
+		assert_not @user.valid?
+	end
+
+	test 'username should not be too short' do
+		@user.username = "aa"
+		assert_not @user.valid?
+	end
+
+	test 'user should have an email' do
+		@user.email = " "
+		assert_not @user.valid?
+	end
+
+	test 'email should be unique' do 
+		@user.save
+		new_user = User.new(email: 't@b.c', username: 'other', password: "123123")
+		assert_not new_user.valid?
+	end
+
+	test 'email uniqueness should not be case sensitive' do
+		@user.save
+		new_user = User.new(email: 'T@b.c', username: 'TesTer', password: "123123")
+		assert_not new_user.valid?
+	end
+
+	test 'email should not be too long' do
+		@user.email = "a" * 102 + "@b.c"
+		assert_not @user.valid?
+	end
+
+	test 'email should not be too short' do
+		@user.email = "a@"
+		assert_not @user.valid?
+	end
+
+	test 'email should have correct email format' do
+		@user.email = "a@b"
+		assert_not @user.valid?
+	end
+
+
 end
