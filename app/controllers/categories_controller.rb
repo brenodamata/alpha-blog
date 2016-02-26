d5059d104fe2f49ef7c53aa368a602f10ff8a2f2
brenodamata <bjrdamata@gmail.com>
2016-02-26 15:26:31 -0500

    views for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,15 +1,31 @@
 class CategoriesController < ApplicationController
 
 	def index
-		
+		@categories = Category.all
 	end
 
 	def new
-		
+    @category = Category.new
+	end
+
+	def create
+		@category = Category.new(category_params)
+		if @category.save
+			flash[:success] = "New '#{@category.name}' category created."
+			redirect_to categories_path
+		else
+			render 'new'
+		end
 	end
 
 	def show
 		
+	end
+
+	protected
+
+	def category_params
+		params.require(:category).permit(:name)
 	end
 
 end
